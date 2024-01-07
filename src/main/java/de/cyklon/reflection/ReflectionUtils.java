de702ae6a02578234cede8a5e4641c930dd91a21
cyklon73 <cyklon698@gmail.com>
2024-01-07 16:54:05 +0100

    implemented getClass method

--- src/main/java/de/cyklon/reflection/ReflectionUtils.java
+++ src/main/java/de/cyklon/reflection/ReflectionUtils.java
@@ -1,8 +1,20 @@
 package de.cyklon.reflection;
 
+import de.cyklon.reflection.exception.NotFoundException;
+import org.jetbrains.annotations.NotNull;
+
 public final class ReflectionUtils {
 
 	private ReflectionUtils() {}
 
+	public static Class<?> getClass(@NotNull String packageName, @NotNull String className) throws NotFoundException {
+		int i = className.lastIndexOf('.');
+		String path = String.format("%s.%s", packageName, className.substring(0, i==-1 ? className.length() : i));
+		try {
+			return Class.forName(path);
+		} catch (ClassNotFoundException e) {
+			throw new NotFoundException(path);
+		}
+	}
 
 }
