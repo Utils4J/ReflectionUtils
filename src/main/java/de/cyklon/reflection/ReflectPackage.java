325478995e370c0ddc970837c282be6ee91618ac
MineKing <mineking9534@gmail.com>
2024-01-07 17:49:04 +0100

    Make ReflectPackage implement MemberContainer

--- src/main/java/de/cyklon/reflection/ReflectPackage.java
+++ src/main/java/de/cyklon/reflection/ReflectPackage.java
@@ -1,5 +1,7 @@
 package de.cyklon.reflection;
 
+import de.cyklon.reflection.entities.MemberContainer;
+import de.cyklon.reflection.entities.ReflectClass;
 import de.cyklon.reflection.function.Filter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
@@ -7,8 +9,7 @@ import org.jetbrains.annotations.Unmodifiable;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public interface ReflectPackage extends Annotatable, Nameable {
-
+public interface ReflectPackage extends Annotatable, Nameable, MemberContainer<Object> {
 
 	@NotNull
 	static ReflectPackage get(@NotNull String packageName) {
@@ -17,11 +18,11 @@ public interface ReflectPackage extends Annotatable, Nameable {
 
 	@NotNull
 	@Unmodifiable
-	Set<Class<?>> getClasses();
+	Set<? extends ReflectClass<?>> getClasses();
 
 	@NotNull
 	@Unmodifiable
-	default Set<Class<?>> getClasses(@NotNull Filter<Class<?>> filter) {
+	default Set<? extends ReflectClass<?>> getClasses(@NotNull Filter<ReflectClass<?>> filter) {
 		return getClasses().stream()
 				.filter(filter::filter)
 				.collect(Collectors.toUnmodifiableSet());
--- src/main/java/de/cyklon/reflection/ReflectPackageImpl.java
+++ src/main/java/de/cyklon/reflection/ReflectPackageImpl.java
@@ -1,6 +1,11 @@
 package de.cyklon.reflection;
 
+import de.cyklon.reflection.entities.ReflectClass;
+import de.cyklon.reflection.entities.ReflectConstructor;
+import de.cyklon.reflection.entities.ReflectField;
+import de.cyklon.reflection.entities.ReflectMethod;
 import de.cyklon.reflection.exception.NotFoundException;
+import de.cyklon.reflection.function.Filter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
 
@@ -14,21 +19,46 @@ import java.util.stream.Collectors;
 
 class ReflectPackageImpl implements ReflectPackage {
 
-
 	private final Package pkg;
 
-	private ReflectPackageImpl(String packageName) {
+	private ReflectPackageImpl(@NotNull String packageName) {
 		this.pkg = Package.getPackage(packageName);
 	}
 
-	static ReflectPackage get(String packageName) {
+	@NotNull
+	static ReflectPackage get(@NotNull String packageName) {
 		return new ReflectPackageImpl(packageName);
 	}
 
 	@NotNull
 	@Override
+	public Set<? extends ReflectConstructor<Object>> getConstructors(@NotNull Filter<ReflectConstructor<Object>> filter) {
+		return getClasses().stream()
+				.flatMap(c -> c.getConstructors(filter).stream())
+				.collect(Collectors.toSet());
+	}
+
+	@NotNull
+	@Override
+	public Set<? extends ReflectMethod<Object, ?>> getMethods(@NotNull Filter<ReflectMethod<Object, ?>> filter) {
+		return getClasses().stream()
+				.flatMap(c -> c.getMethods(filter).stream())
+				.collect(Collectors.toSet());
+	}
+
+	@NotNull
+	@Override
+	public Set<? extends ReflectField<Object, ?>> getFields(@NotNull Filter<ReflectField<Object, ?>> filter) {
+		return getClasses().stream()
+				.flatMap(c -> c.getFields(filter).stream())
+				.collect(Collectors.toSet());
+	}
+
+
+	@NotNull
+	@Override
 	@Unmodifiable
-	public Set<Class<?>> getClasses() {
+	public Set<? extends ReflectClass<?>> getClasses() {
 		String packageName = getName().replaceAll("\\.", "/");
 		try (InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream(packageName)) {
 			if (in==null) throw new NotFoundException(packageName);
@@ -36,7 +66,7 @@ class ReflectPackageImpl implements ReflectPackage {
 			try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
 				return reader.lines()
 						.filter(l -> l.endsWith(".class"))
-						.map(l -> ReflectionUtils.getClass(packageName, l))
+						.map(l -> ReflectClass.wrap(ReflectionUtils.getClass(packageName, l)))
 						.collect(Collectors.toUnmodifiableSet());
 			}
 		} catch (IOException e) {
