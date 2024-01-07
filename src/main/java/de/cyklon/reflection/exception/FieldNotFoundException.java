a8c4951769329a9c760f2da4e7207858f81cd130
MineKing <mineking9534@gmail.com>
2024-01-07 15:59:44 +0100

    Improve exception hierarchic

--- src/main/java/de/cyklon/reflection/exception/FieldNotFoundException.java
+++ src/main/java/de/cyklon/reflection/exception/FieldNotFoundException.java
@@ -4,6 +4,6 @@ import org.jetbrains.annotations.NotNull;
 
 public class FieldNotFoundException extends NotFoundException {
 	public FieldNotFoundException(@NotNull Class<?> parent, @NotNull String name) {
-		super(parent, name);
+		super(name);
 	}
 }
