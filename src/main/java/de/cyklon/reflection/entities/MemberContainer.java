1e71fd5d6e29f7f8416af3454568b5b2a279cf5b
cyklon73 <cyklon698@gmail.com>
2024-01-07 18:34:46 +0100

    changed DeclaringClass in filter to ? because Java

--- src/main/java/de/cyklon/reflection/entities/MemberContainer.java
+++ src/main/java/de/cyklon/reflection/entities/MemberContainer.java
@@ -7,11 +7,11 @@ import java.util.Set;
 
 public interface MemberContainer<D> {
 	@NotNull
-	Set<? extends ReflectConstructor<D>> getConstructors(@NotNull Filter<ReflectConstructor<D>> filter);
+	Set<? extends ReflectConstructor<D>> getConstructors(@NotNull Filter<ReflectConstructor<?>> filter);
 
 	@NotNull
-	Set<? extends ReflectMethod<D, ?>> getMethods(@NotNull Filter<ReflectMethod<D, ?>> filter);
+	Set<? extends ReflectMethod<D, ?>> getMethods(@NotNull Filter<ReflectMethod<?, ?>> filter);
 
 	@NotNull
-	Set<? extends ReflectField<D, ?>> getFields(@NotNull Filter<ReflectField<D, ?>> filter);
+	Set<? extends ReflectField<D, ?>> getFields(@NotNull Filter<ReflectField<?, ?>> filter);
 }
