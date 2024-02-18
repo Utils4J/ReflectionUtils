48e2b852c4555bdea23029db8de7b9f64e2bdb35
cyklon73 <cyklon698@gmail.com>
2024-02-18 12:51:06 +0100

    implemented methods from Loadable interface, and getLoaded methods

--- src/main/java/de/cyklon/reflection/entities/impl/ReflectPackageImpl.java
+++ src/main/java/de/cyklon/reflection/entities/impl/ReflectPackageImpl.java
@@ -1,14 +1,17 @@
 package de.cyklon.reflection.entities.impl;
 
-import de.cyklon.reflection.ReflectionUtils;
+import de.cyklon.reflection.entities.ClassFile;
+import de.cyklon.reflection.entities.OfflinePackage;
 import de.cyklon.reflection.entities.ReflectClass;
 import de.cyklon.reflection.entities.ReflectPackage;
 import de.cyklon.reflection.entities.members.ReflectConstructor;
 import de.cyklon.reflection.entities.members.ReflectField;
 import de.cyklon.reflection.entities.members.ReflectMethod;
 import de.cyklon.reflection.exception.NotFoundException;
+import de.cyklon.reflection.exception.NotLoadedException;
 import de.cyklon.reflection.function.Filter;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Unmodifiable;
 
 import java.io.BufferedReader;
@@ -16,19 +19,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.annotation.Annotation;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class ReflectPackageImpl implements ReflectPackage {
-
-	private final String packageName;
+public class ReflectPackageImpl extends OfflinePackageImpl implements ReflectPackage {
 	private final Package pkg;
 
 	private ReflectPackageImpl(@NotNull String packageName) {
-		this.packageName = packageName;
-		this.pkg = ClassLoader.getSystemClassLoader().getDefinedPackage(packageName);
-		if (pkg == null && checkPackage(packageName)) throw new NotFoundException(packageName, "package", "");
+		super(packageName);
+		this.pkg = getPackage(packageName);
+		if (!isLoaded()) throw new NotLoadedException(packageName, "package", "");
+		if (checkPackage(packageName)) throw new NotFoundException(packageName, "package", "");
 	}
 
 	@NotNull
@@ -38,13 +39,19 @@ public class ReflectPackageImpl implements ReflectPackage {
 		return packageName.isBlank() ? ReflectPackage.BASE_PACKAGE : new ReflectPackageImpl(packageName);
 	}
 
-	//returns true if package was not found
-	private static boolean checkPackage(String packageName) {
-		try (InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream(packageName.replaceAll("\\.", "/"))) {
-			return in==null;
-		} catch (IOException e) {
-			return true;
-		}
+	@NotNull
+	@Override
+	@Unmodifiable
+	public Set<? extends ReflectClass<?>> getLoadedClasses() {
+		return getClasses().stream()
+				.filter(ClassFile::isLoaded)
+				.map(c -> ReflectClass.forName(c.getName()))
+				.collect(Collectors.toUnmodifiableSet());
+	}
+
+	@Nullable
+	static Package getPackage(@NotNull String packageName) {
+		return ClassLoader.getSystemClassLoader().getDefinedPackage(packageName);
 	}
 
 	@Override
@@ -52,8 +59,9 @@ public class ReflectPackageImpl implements ReflectPackage {
 		return pkg != null;
 	}
 
-	private void checkLoaded() {
-		if (!isLoaded()) throw new IllegalStateException(String.format("Can not execute this method to an unloaded package. Package %s is not loaded yet", packageName));
+	@Override
+	public ReflectPackage load() {
+		return this;
 	}
 
 	@NotNull
@@ -61,7 +69,7 @@ public class ReflectPackageImpl implements ReflectPackage {
 	@Unmodifiable
 	@SuppressWarnings("unchecked")
 	public Set<? extends ReflectConstructor<Object>> getConstructors(@NotNull Filter<ReflectConstructor<?>> filter) {
-		return (Set<? extends ReflectConstructor<Object>>) getClasses().stream()
+		return (Set<? extends ReflectConstructor<Object>>) getLoadedClasses().stream()
 				.flatMap(c -> c.getConstructors(filter).stream())
 				.collect(Collectors.toUnmodifiableSet());
 	}
@@ -71,7 +79,7 @@ public class ReflectPackageImpl implements ReflectPackage {
 	@Unmodifiable
 	@SuppressWarnings("unchecked")
 	public Set<? extends ReflectMethod<Object, ?>> getMethods(@NotNull Filter<ReflectMethod<?, ?>> filter) {
-		return (Set<? extends ReflectMethod<Object, ?>>) getClasses().stream()
+		return (Set<? extends ReflectMethod<Object, ?>>) getLoadedClasses().stream()
 				.flatMap(c -> c.getMethods(filter).stream())
 				.collect(Collectors.toUnmodifiableSet());
 	}
@@ -81,48 +89,31 @@ public class ReflectPackageImpl implements ReflectPackage {
 	@Unmodifiable
 	@SuppressWarnings("unchecked")
 	public Set<? extends ReflectField<Object, ?>> getFields(@NotNull Filter<ReflectField<?, ?>> filter) {
-		return (Set<? extends ReflectField<Object, ?>>) getClasses().stream()
+		return (Set<? extends ReflectField<Object, ?>>) getLoadedClasses().stream()
 				.flatMap(c -> c.getFields(filter).stream())
 				.collect(Collectors.toUnmodifiableSet());
 	}
 
+	@NotNull
 	@Override
-	public @NotNull Package getPackage() {
-		checkLoaded();
+	public Package getPackage() {
 		return pkg;
 	}
 
 	@NotNull
 	@Override
 	@Unmodifiable
-	public Set<? extends ReflectClass<?>> getClasses() {
-		return getClasses(getName()).stream()
-				.map(ReflectClass::wrap)
+	public Set<? extends ReflectPackage> getLoadedPackages() {
+		return getPackages().stream()
+				.filter(OfflinePackage::isLoaded)
+				.map(p -> get(p.getName()))
 				.collect(Collectors.toUnmodifiableSet());
-	}
-
-	private static Set<Class<?>> getClasses(String packageName) {
-		try (InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream(packageName.replaceAll("\\.", "/"))) {
-			if (in == null) throw new NotFoundException(packageName, "package", "");
-
-			try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-				Set<Class<?>> result = new HashSet<>();
-				String line;
-				while ((line = reader.readLine()) != null) {
-					if (line.endsWith(".class")) result.add(ReflectionUtils.getClass(packageName, line));
-					else if (!line.contains(".")) result.addAll(getClasses(String.format("%s.%s", packageName, line)));
-				}
-				return result;
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
 	}
 
 	@NotNull
 	@Override
 	@Unmodifiable
-	public Set<? extends ReflectPackage> getPackages() {
+	public Set<? extends OfflinePackage> getPackages() {
 		String packageName = getName();
 		try (InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream(packageName.replaceAll("\\.", "/"))) {
 			if (in == null) throw new NotFoundException(packageName, "package", "");
@@ -130,7 +121,7 @@ public class ReflectPackageImpl implements ReflectPackage {
 			try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
 				return reader.lines()
 						.filter(l -> !l.contains("."))
-						.map(l -> get(String.format("%s.%s", packageName, l)))
+						.map(l -> OfflinePackage.get(String.format("%s.%s", packageName, l)))
 						.collect(Collectors.toUnmodifiableSet());
 			}
 		} catch (IOException e) {
@@ -147,28 +138,22 @@ public class ReflectPackageImpl implements ReflectPackage {
 
 	@Override
 	public Annotation[] getAnnotations() {
-		checkLoaded();
 		return pkg.getAnnotations();
 	}
 
 	@Override
 	public Annotation[] getDeclaredAnnotations() {
-		checkLoaded();
 		return pkg.getDeclaredAnnotations();
 	}
 
+	@NotNull
 	@Override
-	public @NotNull String getName() {
-		return isLoaded() ? pkg.getName() : packageName;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		return obj instanceof ReflectPackage rp && rp.getName().equals(getName());
+	public String getName() {
+		return pkg.getName();
 	}
 
 	@Override
 	public String toString() {
-		return isLoaded() ? pkg.toString() : String.format("package %s", packageName);
+		return pkg.toString();
 	}
 }
