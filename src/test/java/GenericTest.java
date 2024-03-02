ab4843258931496c1d420596a74b9eecc52d2b57
MineKing <mineking9534@gmail.com>
2024-03-02 18:19:02 +0100

    add component type test

--- src/test/java/GenericTest.java
+++ src/test/java/GenericTest.java
@@ -3,20 +3,33 @@ import de.cyklon.reflection.entities.members.ReflectField;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class GenericTest {
-	public final List<String>[][][] test = null;
+	public final List<String>[][][] array = null;
+	public final Map<String, List<Integer>> map = null;
 
 	@Test
 	public void array() {
-		ReflectField<?, ?> field = ReflectClass.wrap(GenericTest.class).getField("test");
+		ReflectField<?, ?> field = ReflectClass.wrap(GenericTest.class).getField("array");
 		ReflectClass<?> type = field.getReturnType();
 		ReflectClass.ArrayInfo array = type.getArrayInfo();
 
 		assertEquals("java.util.List<java.lang.String>", array.component().toString());
 		assertEquals(3, array.depth());
+	}
+
+	@Test
+	public void component() {
+		ReflectField<?, ?> field = ReflectClass.wrap(GenericTest.class).getField("map");
+		ReflectClass<?> type = field.getReturnType();
+		ReflectClass<?> key = type.getTypeParameters().get(0);
+		ReflectClass<?> value = type.getTypeParameters().get(1);
+
+		assertEquals("java.lang.String", key.toString());
+		assertEquals("java.util.List<java.lang.Integer>", value.toString());
 	}
 }
