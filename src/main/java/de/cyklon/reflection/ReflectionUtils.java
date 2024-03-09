1cc8f45e009b4d803b050753dd63dce30c3ab967
MineKing <mineking9534@gmail.com>
2024-03-09 18:41:19 +0100

    Use correct exception for ReflectionUtils#getClass

--- src/main/java/de/cyklon/reflection/ReflectionUtils.java
+++ src/main/java/de/cyklon/reflection/ReflectionUtils.java
@@ -1,6 +1,7 @@
 package de.cyklon.reflection;
 
-import de.cyklon.reflection.exception.NotFoundException;
+import de.cyklon.reflection.exception.ClassNotFoundException;
+import de.cyklon.reflection.types.Nameable;
 import org.jetbrains.annotations.NotNull;
 
 public final class ReflectionUtils {
@@ -8,16 +9,18 @@ public final class ReflectionUtils {
 	private ReflectionUtils() {
 	}
 
-	public static Class<?> getClass(@NotNull String packageName, @NotNull String className) throws NotFoundException {
+	@NotNull
+	public static Class<?> getClass(@NotNull String packageName, @NotNull String className) throws ClassNotFoundException {
 		int i = className.lastIndexOf('.');
 		return getClass(String.format("%s.%s", packageName, className.substring(0, i == -1 ? className.length() : i)));
 	}
 
-	public static Class<?> getClass(@NotNull String className) {
+	@NotNull
+	public static Class<?> getClass(@NotNull String className) throws ClassNotFoundException {
 		try {
 			return Class.forName(className);
-		} catch (ClassNotFoundException e) {
-			throw new NotFoundException(className, "class", className.substring(0, className.lastIndexOf('.')));
+		} catch (java.lang.ClassNotFoundException e) {
+			throw new ClassNotFoundException(Nameable.wrap(className.substring(0, className.lastIndexOf('.'))), className);
 		}
 	}
 
