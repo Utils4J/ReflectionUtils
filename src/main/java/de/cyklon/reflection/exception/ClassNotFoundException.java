03c4e74dcb649f9b898e9ea80be7680ba4766c2e
cyklon73 <cyklon698@gmail.com>
2024-03-09 12:34:56 +0100

    set Nameable as Nullable

--- src/main/java/de/cyklon/reflection/exception/ClassNotFoundException.java
+++ src/main/java/de/cyklon/reflection/exception/ClassNotFoundException.java
@@ -2,9 +2,10 @@ package de.cyklon.reflection.exception;
 
 import de.cyklon.reflection.types.Nameable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class ClassNotFoundException extends NotFoundException {
-	public ClassNotFoundException(@NotNull Nameable parent, @NotNull String name) {
-		super(name, "class", parent.getName());
+	public ClassNotFoundException(@Nullable Nameable parent, @NotNull String name) {
+		super(name, "class", parent==null ? "" : parent.getName());
 	}
 }
